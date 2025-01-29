52c6e0ff7ad5499f95d186d30ef22264eca4b336
sayed-2299 <osayed2299@gmail.com>
2025-01-29 23:16:03 +0600

    Update task_1.py

--- Artificial Intelligence (CSE-316)/CLP_1/task_1.py
+++ Artificial Intelligence (CSE-316)/CLP_1/task_1.py
@@ -1,1 +1,13 @@
+numbers=map(int,input('Enter the numbers:').split())
 
+sum_of_odd =0
+sum_of_even=0
+
+for number in numbers:
+    if number % 2 == 0:
+        sum_of_even += number
+    else:
+        sum_of_odd += number
+
+print('sum of even:',sum_of_even)
+print('sum of odd:',sum_of_odd)
